34501d75923a0c8357325ddec5f3d6427465ce0d
Bykov Dima <fepifmel@gmail.com>
2020-03-06 00:40:19 +0300

    добавлены секции footer,containers,geography,contact,map,parnters

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,6 +1,6 @@
-// window.addEventListener("scroll", () => {
-//   let parallaxBg = document.querySelector(".js-parallax-bg");
-//   let y = window.pageYOffset / 2 + "px";
+window.addEventListener("scroll", () => {
+  let parallaxBg = document.querySelector(".js-parallax-bg");
+  let y = window.pageYOffset / 2 + "px";
 
-//   parallaxBg.style.setProperty("--bg-positionY", y);
-// });
+  parallaxBg.style.setProperty("--bg-positionY", y);
+});
